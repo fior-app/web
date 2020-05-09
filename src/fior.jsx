ca7b4e79a651704f605287a746c2c5ac82714438
Sashika Nawarathne <hi@sashika.me>
2020-05-09 23:09:20 +0530

    Install semantic & redux

---
+++ src/fior.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import MainScreen from "./screens/main_screen";
+
+function Fior() {
+  return <MainScreen />;
+}
+
+export default Fior;
---
+++ src/screens/main_screen.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class MainScreen extends Component {
+  state = {};
+  render() {
+    return <h1 className="heading">Welcome</h1>;
+  }
+}
+
+export default MainScreen;
