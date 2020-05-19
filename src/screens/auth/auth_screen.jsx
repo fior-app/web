effc393abfb923e8c2de55cab8ea0e0dec42a29e
Sashika Nawarathne <hi@sashika.me>
2020-05-19 14:50:42 +0530

    Update placeholder & UI

--- src/screens/auth/auth_screen.jsx
+++ src/screens/auth/auth_screen.jsx
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 import Login from "../../components/auth/login/login_cmp";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from "react-router-dom";
 import "./auth.css";
 import Register from "../../components/auth/register/register_cmp";
+import { Image } from "semantic-ui-react";
 
 class AuthScreen extends Component {
   state = {};
@@ -10,17 +16,28 @@ class AuthScreen extends Component {
     return (
       <React.Fragment>
         <div className='container'>
-          <div className='row'>
-            <div className='placeholder'></div>
+          <div className='row j-around'>
+            <div className='placeholder'>
+              <Image src='../../assets/img/auth.svg' size='massive' />
+            </div>
+            {/* <div className='spacer'></div> */}
             <Router>
               <div className='items-right'>
                 <div className='row menu'>
-                  <Link to='/login'>
-                    <div className='menu-item'>Login</div>
-                  </Link>
-                  <Link to='/login/welcome'>
-                    <div className='menu-item'>Register</div>
-                  </Link>
+                  <div className='menu-item'>
+                    <NavLink to='/login' exact activeClassName='selected'>
+                      Login
+                    </NavLink>
+                  </div>
+                  <div className='menu-item'>
+                    <NavLink
+                      to='/login/welcome'
+                      exact
+                      activeClassName='selected'
+                    >
+                      Register
+                    </NavLink>
+                  </div>
                 </div>
                 <Switch>
                   <Route path='/login' exact component={Login} />
