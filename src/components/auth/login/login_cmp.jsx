d8522a3faa42e0767cb27e7e6c7bf4d6ce3ac74b
hirantha <sahanhirantha@gmail.com>
2020-05-21 02:19:08 +0530

    implemented redux, implemented sign in using email

--- src/components/auth/login/login_cmp.jsx
+++ src/components/auth/login/login_cmp.jsx
@@ -1,42 +1,70 @@
 import React, { Component } from "react";
 import { Card, Form, Button, Icon, Divider } from "semantic-ui-react";
+import { connect } from "react-redux";
+
 import "./login.css";
+import { signInEmail } from "../../../store/actions/authActions";
 
 class Login extends Component {
   state = {
     email: "",
     password: "",
   };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.signInEmail(this.state);
+  };
+
   render() {
     return (
-      <div className='center'>
+      <div className="center">
         <Card>
           <Card.Content>
             <Card.Description>
-              <Button color='google plus' fluid>
-                <Icon name='google' />
+              <Button color="google plus" fluid>
+                <Icon name="google" />
                 Sign in with Google
               </Button>
-              <div className='v-spacer'></div>
-              <Button color='linkedin' fluid>
-                <Icon name='linkedin' />
+              <div className="v-spacer"></div>
+              <Button color="linkedin" fluid>
+                <Icon name="linkedin" />
                 Sign in with LinkedIn
               </Button>
-              <div className='v-spacer'></div>
+              <div className="v-spacer"></div>
               <Divider horizontal>OR</Divider>
               {/* <Card.Meta>Enter your email and password to login</Card.Meta> */}
-              <Form>
+              <Form onSubmit={this.handleSubmit}>
                 <Form.Field>
-                  <input placeholder='Email' />
+                  <input
+                    type="email"
+                    required
+                    placeholder="Email"
+                    id="email"
+                    onChange={this.handleChange}
+                  />
                 </Form.Field>
                 <Form.Field>
-                  <input placeholder='Password' />
+                  <input
+                    type="password"
+                    required
+                    placeholder="Password"
+                    id="password"
+                    onChange={this.handleChange}
+                  />
                 </Form.Field>
-                <div className='end'>
-                  <Button type='submit' fluid>
+                <div className="end">
+                  <Button type="submit" fluid>
                     Login
                   </Button>
                 </div>
+                <div>{this.props.auth.authError}</div>
               </Form>
             </Card.Description>
           </Card.Content>
@@ -46,4 +74,14 @@ class Login extends Component {
   }
 }
 
-export default Login;
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signInEmail: (credentials) => dispatch(signInEmail(credentials)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
---
+++ src/store/reducers/authReducer.js
@@ -0,0 +1,22 @@
+const initState = {
+  authError: null,
+};
+
+const authReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "LOGIN_ERROR":
+      return {
+        ...state,
+        authError: "Login failed!",
+      };
+    case "LOGIN_SUCCESS":
+      return {
+        ...state,
+        authError: null,
+      };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
