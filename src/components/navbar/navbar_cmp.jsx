9dcdf56646877594e316e1a9071980b250bc399b
hirantha <mail@hirantha.xyz>
2020-05-23 19:18:08 +0530

    fix token error

--- src/components/navbar/navbar_cmp.jsx
+++ src/components/navbar/navbar_cmp.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+
 import "./navbar.css";
 
 class Navbar extends Component {
   state = {};
   render() {
+    const { user } = this.props;
+
     return (
       <React.Fragment>
         <div className="row">
@@ -13,13 +17,26 @@ class Navbar extends Component {
           <NavLink to="/">
             <div className="nav-item">Home</div>
           </NavLink>
-          <NavLink to="/login">
-            <div className="nav-item">Login</div>
-          </NavLink>
+          {user ? (
+            <NavLink to="/logout">
+              <div className="nav-item">
+                Logout<span> ({user.name.split(" ")[0]})</span>
+              </div>
+            </NavLink>
+          ) : (
+            <NavLink to="/login">
+              <div className="nav-item">Login</div>
+            </NavLink>
+          )}
         </div>
       </React.Fragment>
     );
   }
 }
 
-export default Navbar;
+const mapStateToProps = (state) => ({
+  user: state.auth.currentUser,
+});
+
+const mapDispatchToProps = {};
+export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
