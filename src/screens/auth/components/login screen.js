2cd50c4d8191ba45818740a7cd04551038f7b5fa
Sachie <sachiejayen@gmail.com>
2020-06-05 13:56:49 +0530

    login change

--- src/screens/auth/components/login screen.js
+++ src/screens/auth/components/login screen.js
@@ -7,3 +7,63 @@ import {
 from 'semantic-ui-react';
 import { REACT_APP_GOOGLE_CLIENT_ID } from '../../../config/config';
 import { signInEmail, signInGoogle } from '../../../store/actions/authActions';
+
+class Login extends Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        email: '',
+        password: '',
+      };
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    const mapStateToProps = (state) => ({
+        authState: state.auth.authState,
+      });
+      
+      // Test before use
+      // const mapDispatchToProps = { signInEmail, signInGoogle };
+      
+      const mapDispatchToProps = (dispatch) => ({
+        signInEmail: (credentials) => dispatch(signInEmail(credentials)),
+        signInGoogle: (idToken) => dispatch(signInGoogle(idToken)),
+      });
+      
+      export default connect(mapStateToProps, mapDispatchToProps)(Login);
+      
