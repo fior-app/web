68a77c3bbee25a3ffac80c7b1df84c654b08a0bc
Sashika Nawarathne <hi@sashika.me>
2020-07-05 21:36:34 +0530

    Add custom css

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 
-import "./styles.css";
 import FiorApp from "./fior-app";
 import * as serviceWorker from "./serviceWorker";
 import rootReducer from "./store/reducers/rootReducer";
 import AxiosConfig from "./config/axios-config";
+
+import "./styles.css";
 
 AxiosConfig.config();
 
