0bf87b527e463e4b357c3c36dcfc7699963ab115
Prabhani Nanayakkara <prabhaninanayakkara1907@gmail.com>
2020-07-09 00:11:32 +0530

    UI fixes

--- src/screens/auth/auth_screen.jsx
+++ src/screens/auth/auth_screen.jsx
@@ -48,6 +48,7 @@ class AuthScreen extends Component {
             </div>
           </div>
         </div> */}
+
         <div className='container'>
 
           {/* Background Vectors */}
@@ -64,35 +65,61 @@ class AuthScreen extends Component {
 
           {/* Login and Register Forms */}
           <div className='login-forms row'>
-            <Image src='../../assets/vectors/signin_bg.svg' className='signin_bg' />
+            <Image src='../../assets/vectors/signin_bg.svg' className='signin_bg register_bg' />
 
             {/* Tabs */}
-            <Image src='../../assets/vectors/form_tab_right.svg' className='form_tab_right' />
+            <Image src='../../assets/vectors/form_tab_right.svg' className='form_tab_right form_tab_left' />
             <p className='signin_text'>Sign In</p>
             <p className='register_text'>Register</p>
             {/* Enf of Tabs */}
 
             {/* Form Fields */}
+
+            {/* Signin form fields */}
+            {/* <form> */}
+            {/* <input type="text" name="username" className="username" placeholder="Username" /> */}
+            {/* <input type="password" name="password" className="password" placeholder="Password" /> */}
+            {/* </form> */}
+
+            {/* Register form fields */}
             <form>
-              <input type="text" name="username" className="username" placeholder="Username" />
-              <input type="password" name="password" className="password" placeholder="Password" />
+              <div className='register_name_row row'>
+                <input type="text" name="firstname" className="firstname" placeholder="First Name" />
+                <input type="text" name="lastname" className="lastname" placeholder="Last Name" />
+              </div>
+              <input type="text" name="email" className="email" placeholder="Email" />
+              <input type="password" name="password_register" className="password_register" placeholder="Password" />
+              <div className='allow_tnc row'>
+                <input type="checkbox" name="tnc_checkbox" className='tnc_checkbox' checked="checked" />
+                <span className='checkmark'></span>
+                <label htmlFor="TnC" className='tnc_label'>Agree to Terms and Conditions</label>
+              </div>
             </form>
             {/* End of Form Fields */}
 
             {/* Secondary Btn */}
-            <button className='btn_secondary'>Sign In</button>
+            {/* Sign In button */}
+            {/* <button className='signin_btn secondary_btn'>Sign In</button> */}
+
+            {/* Register button */}
+            <button className='register_btn secondary_btn'>Register</button>
+
             {/* End of Secondary Btn */}
 
-            <p className='or_divider'>OR</p>
+            {/* Only for signin tab */}
+            {/* <p className='or_divider'>OR</p> */}
 
             {/* Google sign in LinkedIn sign in btns */}
-            <button className='google_signin_btn'><Image src='../../assets/icons/flat-color-icons_google.svg' className='google_icon' /><p>SignIn with Google</p></button>
-            <button className='linkedin_signin_btn'><Image src='../../assets/icons/linkedin.svg' className='linkedin_icon' /><p>SignIn with LinkedIn</p></button>
+            <button className='google_signin_btn google_signin google_register'><Image src='../../assets/icons/flat-color-icons_google.svg' className='google_icon' /><p>SignIn with Google</p></button>
+            <button className='linkedin_signin_btn linkedin_signin linkedin_register'><Image src='../../assets/icons/linkedin.svg' className='linkedin_icon' /><p>SignIn with LinkedIn</p></button>
+
+            {/* Add .google_register and .linkedin_register classes when toggling to register tab */}
+            
             {/* End of Google sign in LinkedIn sign in btns */}
 
           </div>
           {/* End of Login and Register Forms */}
-          
+
         </div>
       </React.Fragment>
     );
