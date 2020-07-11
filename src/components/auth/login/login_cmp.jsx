9764abebf72ff67ce87a1af44ec175f1e97902b5
Sashika Nawarathne <hi@sashika.me>
2020-07-12 03:20:53 +0530

    Fix auth screen

--- src/components/auth/login/login_cmp.jsx
+++ src/components/auth/login/login_cmp.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { Card, Form, Button, Icon, Divider } from "semantic-ui-react";
 import { connect } from "react-redux";
+import { Image } from "semantic-ui-react";
 
 import { signInEmail } from "../../../store/actions/authActions";
 
@@ -23,28 +23,45 @@ class Login extends Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          type="email"
-          required
-          placeholder="Email"
-          className="username"
-          id="email"
-          onChange={this.handleChange}
-        />
-        <input
-          type="password"
-          required
-          placeholder="Password"
-          className="password"
-          id="password"
-          onChange={this.handleChange}
-        />
-        <button type="submit" className="signin_btn secondary_btn">
-          Sign In
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type='email'
+            required
+            placeholder='Email'
+            className='username'
+            id='email'
+            onChange={this.handleChange}
+          />
+          <input
+            type='password'
+            required
+            placeholder='Password'
+            className='password'
+            id='password'
+            onChange={this.handleChange}
+          />
+          <button type='submit' className='signin_btn secondary_btn'>
+            Login
+          </button>
+          <div>{this.props.auth.authError}</div>
+        </form>
+        <div className='v-spacer-2' />
+        <button className='google_signin_btn google_signin'>
+          <Image
+            src='../../assets/icons/flat-color-icons_google.svg'
+            className='google_icon'
+          />
+          <p>Login with Google</p>
         </button>
-        <div>{this.props.auth.authError}</div>
-      </form>
+        <button className='linkedin_signin_btn linkedin_signin'>
+          <Image
+            src='../../assets/icons/linkedin.svg'
+            className='linkedin_icon'
+          />
+          <p>Login with LinkedIn</p>
+        </button>
+      </div>
     );
   }
 }
