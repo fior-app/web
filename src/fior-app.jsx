74310d75b28559233564e6159a35dad038c16b0f
нιяαитнα <mail@hirantha.xyz>
2020-07-12 20:38:44 +0530

    Fix module import

--- src/fior-app.jsx
+++ src/fior-app.jsx
@@ -9,7 +9,7 @@ import GroupsScreen from "./screens/groups/GroupsScreen";
 import GroupScreen from "./screens/group/GroupScreen";
 import ProfileScreen from "./screens/profile/profile_screen";
 import BlogScreen from "./screens/blog/BlogScreen";
-import Blogdetail from "./screens/blog/Blogdetail";
+import Blogdetail from "./screens/blog/blogdetail";
 
 import { userMe } from "./store/actions/authActions";
 import Footer from "./components/footer/footer";
