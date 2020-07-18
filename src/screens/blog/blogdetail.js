e72a1b0ac6c644d225a95a723bf4265c2cedf107
Prabhani Nanayakkara <prabhaninanayakkara1907@gmail.com>
2020-07-18 17:16:52 +0530

    Update Blog Details Screen UI

--- src/screens/blog/blogdetail.js
+++ src/screens/blog/blogdetail.js
@@ -1,45 +1,62 @@
 import React, { Component } from 'react';
+import SideNav from '../../components/sidenav/sidenav_cmp';
 class Blogdetail extends Component {
-    state = {  }
-    render() { 
-        return (
-            <div className='container'>
-                <div>
-                <p className='blog_post_title'>Post title</p>
-                <img className='blog_view_image' src='../../assets/vectors/blogviewicon.svg'/>
-                <p className='blog_content_detail'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
-                
+  state = {}
+  render() {
+    return (
+      <div className='container'>
+        <SideNav />
+        <div>
+          <p className='blog_post_title'>Post title</p>
+          <img className='blog_view_image' src='../../assets/vectors/blogviewicon.svg' />
+          <p className='blog_content_detail'>Amet minim mollit non deserunt ullamco est sit aliqua
+          dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam
+          consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor
+          do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat
+          sunt nostrud amet.Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
+          Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud
+          amet.Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
+          Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+          consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+          consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+          consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+          consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+
+        </div>
+        <div className="blog_right_row">
+          <div className='blog_view'>
+            <div className='blog_view_catergory_title'>
+              <p>Related catergories</p>
+              <div className='catergory_type_group'>
+                <div className='blg_view_catergory_type_box1'>
+                  <p className="blog_view_catergory_name">Catergory type </p>
                 </div>
-                <div className="blog_right_row">
-                <div className='blog_view'>
-                    <div className='blog_view_catergory_title'>
-                        <p>Related catergories</p>
-                    <div className='catergory_type_group'>
-                    <div className='blg_view_catergory_type_box1'>
-                    <p className="blog_view_catergory_name">Catergory type </p>
-                    </div>
-                    </div>
-                    </div>
+              </div>
+            </div>
 
-                    <div className='blog_view_resent_posts'>
-                        <p className='blog_view_resent_title'>Resent posts</p>
-                        <div className='blog_view_resent_box'>
-                            <div className='blog_view_resent_detail'>
-                                <p>Post Title : Amet minim mollit non deserunt ullamco</p>
-                            </div>
-                        </div>
-
-                    </div>
+            <div className='blog_view_resent_posts'>
+              <p className='blog_view_resent_title'>Resent posts</p>
+              <div className='blog_view_resent_box'>
+                <div className='blog_view_resent_detail'>
+                  <p>Post Title : Amet minim mollit non deserunt ullamco</p>
                 </div>
+              </div>
 
-                </div>
-                
+            </div>
+          </div>
 
-             </div>
-        );
-    }
+        </div>
+
+
+      </div>
+    );
+  }
 }
- 
+
 
 
 export default Blogdetail;
