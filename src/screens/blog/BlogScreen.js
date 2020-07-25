ee7b55ec0864e2473c4770b1abab0d075c5a53fc
Sashika Nawarathne <hi@sashika.me>
2020-07-25 20:55:16 +0530

    Update sidenav dynamic rendering

--- src/screens/blog/BlogScreen.js
+++ src/screens/blog/BlogScreen.js
@@ -1,26 +1,51 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import SideNav from "../../components/sidenav/sidenav_cmp";
-import WriteBlogScreen from "./WriteBlogScreen";
-import Posts from "./Posts";
-import './blog.css'
+import "./blog.css";
 
 export class BlogScreen extends Component {
   render() {
     return (
-      <div className="container">
+      <div className='container'>
         {/* <SideNav /> */}
 
         <div className='blog_container_right'>
           {/* Blog Header */}
-          <div className="blog_header">
-            <img className="blogicon" src="../../assets/vectors/blogicon.svg" />
-            <img className="blogicon7" src="../../assets/vectors/blogicon7.svg" />
-            <img className="blogicon2" src="../../assets/vectors/blogicon2.svg" />
-            <img className="blogicon5" src="../../assets/vectors/blogicon5.svg" />
-            <img className="blogicon6" src="../../assets/vectors/blogicon6.svg" />
-            <img className="blogicon4" src="../../assets/vectors/blogicon4.svg" />
-            <img className="blogicon3" src="../../assets/vectors/blogicon3.svg" />
+          <div className='blog_header'>
+            <img
+              className='blogicon'
+              src='../../assets/vectors/blogicon.svg'
+              alt='design'
+            />
+            <img
+              className='blogicon7'
+              src='../../assets/vectors/blogicon7.svg'
+              alt='design'
+            />
+            <img
+              className='blogicon2'
+              src='../../assets/vectors/blogicon2.svg'
+              alt='design'
+            />
+            <img
+              className='blogicon5'
+              src='../../assets/vectors/blogicon5.svg'
+              alt='design'
+            />
+            <img
+              className='blogicon6'
+              src='../../assets/vectors/blogicon6.svg'
+              alt='design'
+            />
+            <img
+              className='blogicon4'
+              src='../../assets/vectors/blogicon4.svg'
+              alt='design'
+            />
+            <img
+              className='blogicon3'
+              src='../../assets/vectors/blogicon3.svg'
+              alt='design'
+            />
           </div>
           {/* End of Blog Header */}
 
@@ -32,118 +57,113 @@ export class BlogScreen extends Component {
 
           {/* Blog Content */}
           <div className='blog_content'>
-
             {/* Mid Col Blog Items */}
-            <div className="blog_items_col">
-
+            <div className='blog_items_col'>
               {/* Blog Item Row */}
-              <div className="blog_item">
-
+              <div className='blog_item'>
                 {/* Blog Date */}
-                <div className="blog_date_bg">
-                  <p className="blog_date">date of post</p>
+                <div className='blog_date_bg'>
+                  <p className='blog_date'>date of post</p>
                 </div>
                 {/* End of Blog Date */}
 
                 {/* Blog Card */}
-                <div className="blog_card">
-                  <img className="blog_image" src="../../assets/vectors/blogimage.svg" />
-                  <div className="blog_card_catergory">
+                <div className='blog_card'>
+                  <img
+                    className='blog_image'
+                    src='../../assets/vectors/blogimage.svg'
+                  />
+                  <div className='blog_card_catergory'>
                     <p>Catergory</p>
-                    <p className="post_title">Post title</p>
-                    <p className="post_meta">Here is the content of the blog</p>
-                    <p className="post_author">Author</p>
+                    <p className='post_title'>Post title</p>
+                    <p className='post_meta'>Here is the content of the blog</p>
+                    <p className='post_author'>Author</p>
                   </div>
                 </div>
                 {/* End of Blog Card */}
-
               </div>
               {/* End of Blog Item Row */}
 
               {/* Blog Item Row */}
-              <div className="blog_item">
-
+              <div className='blog_item'>
                 {/* Blog Date */}
-                <div className="blog_date_bg">
-                  <p className="blog_date">date of post</p>
+                <div className='blog_date_bg'>
+                  <p className='blog_date'>date of post</p>
                 </div>
                 {/* End of Blog Date */}
 
                 {/* Blog Card */}
-                <div className="blog_card">
-                  <img className="blog_image" src="../../assets/vectors/blogimage.svg" />
-                  <div className="blog_card_catergory">
+                <div className='blog_card'>
+                  <img
+                    className='blog_image'
+                    src='../../assets/vectors/blogimage.svg'
+                  />
+                  <div className='blog_card_catergory'>
                     <p>Catergory</p>
-                    <p className="post_title">Post title</p>
-                    <p className="post_meta">Here is the content of the blog</p>
-                    <p className="post_author">Author</p>
+                    <p className='post_title'>Post title</p>
+                    <p className='post_meta'>Here is the content of the blog</p>
+                    <p className='post_author'>Author</p>
                   </div>
                 </div>
                 {/* End of Blog Card */}
-
               </div>
               {/* End of Blog Item Row */}
 
               {/* Blog Item Row */}
-              <div className="blog_item">
-
+              <div className='blog_item'>
                 {/* Blog Date */}
-                <div className="blog_date_bg">
-                  <p className="blog_date">date of post</p>
+                <div className='blog_date_bg'>
+                  <p className='blog_date'>date of post</p>
                 </div>
                 {/* End of Blog Date */}
 
                 {/* Blog Card */}
-                <div className="blog_card">
-                  <img className="blog_image" src="../../assets/vectors/blogimage.svg" />
-                  <div className="blog_card_catergory">
+                <div className='blog_card'>
+                  <img
+                    className='blog_image'
+                    src='../../assets/vectors/blogimage.svg'
+                  />
+                  <div className='blog_card_catergory'>
                     <p>Catergory</p>
-                    <p className="post_title">Post title</p>
-                    <p className="post_meta">Here is the content of the blog</p>
-                    <p className="post_author">Author</p>
+                    <p className='post_title'>Post title</p>
+                    <p className='post_meta'>Here is the content of the blog</p>
+                    <p className='post_author'>Author</p>
                   </div>
                 </div>
                 {/* End of Blog Card */}
-
               </div>
               {/* End of Blog Item Row */}
-
             </div>
             {/* End of Mid Col Blog Items */}
 
             {/* Right Nav */}
-            <div className="blog_right_row">
-
+            <div className='blog_right_row'>
               <button className='icon_btn secondary_btn'>
                 <img src='../../assets/icons/add.svg' />
-              Create new post
+                Create new post
               </button>
 
-              <div className="blog_catergory">
-                <p className="catergory_title1">Catergory</p>
+              <div className='blog_catergory'>
+                <p className='catergory_title1'>Catergory</p>
 
                 <div className='catergory_types'>
-                  <div className="catergory_name">
+                  <div className='catergory_name'>
                     <p>Catergory type</p>
                   </div>
                 </div>
-
               </div>
 
-              <div className="blog_recent_posts">
-                <p className="recent_blog_title1">Recent Posts</p>
+              <div className='blog_recent_posts'>
+                <p className='recent_blog_title1'>Recent Posts</p>
 
                 <div className='recent_posts_types'>
-                  <div className="recent_post_title1">
+                  <div className='recent_post_title1'>
                     <p>post title</p>
                   </div>
                 </div>
-
               </div>
-
             </div>
             {/* End of Right Nav */}
-
           </div>
           {/* End of Blog Content */}
 
@@ -151,12 +171,11 @@ export class BlogScreen extends Component {
           <div className='pagination'>
             <div className='pagination_content'>
               <button>Prev</button>
-              <input type='number'/>              
+              <input type='number' />
               <button>Next</button>
             </div>
           </div>
           {/* End of Pagination */}
-
         </div>
       </div>
     );
