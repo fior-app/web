9c846ac2c7dc0aad41ded4affb054dacb84cd46e
UdeshUK <mail@udesh.xyz>
2020-08-24 15:35:31 +0530

    Fixed sidenav lint errors

--- src/components/sidenav/sidenav_cmp.js
+++ src/components/sidenav/sidenav_cmp.js
@@ -1,70 +1,59 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-class SideNav extends Component {
-  render() {
-    const { user } = this.props;
+const SideNav = (user) => (
+  <>
+    <div className="sidenav_container">
+      <NavLink to="/">
+        <img src="../../assets/logo/logo.svg" alt="Fior Logo" />
+      </NavLink>
 
-    return (
-      <>
-        <div className="sidenav_container">
-          <NavLink to="/">
-            <img src="../../assets/logo/logo.svg" alt="Fior Logo" />
+      <div className="sidenav_items">
+        <NavLink to="/">
+          <button type="button" className="btn-primary">Home</button>
+        </NavLink>
+        {user && (
+          <NavLink to="/mentorspaces">
+            <button type="button" className="btn-primary">Mentorspace</button>
           </NavLink>
-
-          <div className="sidenav_items">
-            <NavLink to="/">
-              <button className="btn-primary">Home</button>
-            </NavLink>
-            {user && (
-              <NavLink to="/mentorspaces">
-                <button className="btn-primary">Mentorspace</button>
-              </NavLink>
-            )}
-            {user && (
-              <NavLink to="/orgs">
-                <button className="btn-primary">Organizations</button>
-              </NavLink>
-            )}
-            <NavLink to="question-forum">
-              <button className="btn-primary">Question Forum</button>
-            </NavLink>
-            <NavLink to="/blog">
-              <button className="btn-primary">Blog</button>
-            </NavLink>
-            {user && (
-              <NavLink to="/users">
-                <button className="btn-primary">Users</button>
-              </NavLink>
-            )}
-            {user && (
-              <NavLink to="/notifications">
-                <button className="btn-primary">Notifications</button>
-              </NavLink>
-            )}
-            {user && (
-              <NavLink to="/settings">
-                <button className="btn-primary">Settings</button>
-              </NavLink>
-            )}
-            <NavLink to="/pricing">
-              <button className="btn-primary ">Pricing Plan</button>
-            </NavLink>
-            <NavLink to="about">
-              <button className="btn-primary ">About Us</button>
-            </NavLink>
-          </div>
-
-          {/* authenticated: display only for registered users. if not, style{display:none; visibility:none} */}
-          {/* unauthenticated: display only for public users. if not, style{display:none; visibility:none} */}
-
-          {/* sidenav_container: style{display:none; visibility:none} for landing screen and login screen - Already done */}
-        </div>
-      </>
-    );
-  }
-}
+        )}
+        {user && (
+          <NavLink to="/orgs">
+            <button type="button" className="btn-primary">Organizations</button>
+          </NavLink>
+        )}
+        <NavLink to="question-forum">
+          <button type="button" className="btn-primary">Question Forum</button>
+        </NavLink>
+        <NavLink to="/blog">
+          <button type="button" className="btn-primary">Blog</button>
+        </NavLink>
+        {user && (
+          <NavLink to="/users">
+            <button type="button" className="btn-primary">Users</button>
+          </NavLink>
+        )}
+        {user && (
+          <NavLink to="/notifications">
+            <button type="button" className="btn-primary">Notifications</button>
+          </NavLink>
+        )}
+        {user && (
+          <NavLink to="/settings">
+            <button type="button" className="btn-primary">Settings</button>
+          </NavLink>
+        )}
+        <NavLink to="/pricing">
+          <button type="button" className="btn-primary ">Pricing Plan</button>
+        </NavLink>
+        <NavLink to="about">
+          <button type="button" className="btn-primary ">About Us</button>
+        </NavLink>
+      </div>
+    </div>
+  </>
+);
 
 const mapStateToProps = (state) => ({
   user: state.auth.currentUser,
