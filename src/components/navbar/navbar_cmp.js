5106b89d99c2acfcf861addc29bbe9b6ad278a4c
Sashika Nawarathne <hi@sashika.me>
2020-08-29 12:53:03 +0530

    Update navbar

--- src/components/navbar/navbar_cmp.js
+++ src/components/navbar/navbar_cmp.js
@@ -1,100 +1,76 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
-import { signOut } from '../../store/actions/authActions';
-
-// const options = ["Profile", "Sign Out"];
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { signOut } from "../../store/actions/authActions";
+import { withRouter } from "react-router-dom";
+import { Menu, Image, Dropdown } from "semantic-ui-react";
+import styles from "../../styles/navbar.module.css";
 
 class Navbar extends Component {
-  state = {
-    avatarUrl: 'https://i.ytimg.com/vi/9K46DNoE3Ko/maxresdefault.jpg',
-  };
+  state = {};
 
   render() {
     const { user } = this.props;
 
-    // return (
-    //   <React.Fragment>
-    //     <div className='row navbar v-align'>
-    //       <NavLink to='/'>
-    //         <div className='logo'>
-    //           <Image src='../../assets/logo/logo.png' />
-    //         </div>
-    //       </NavLink>
-    //       <div className='searchbar'>
-    //         <div>Search</div> <div className='spacer' /> <Icon name='search' />
-    //       </div>
-    //       <div className='spacer'></div>
-    //       {user ? this.renderSignedInLinks() : this.renderSignedOutLinks()}
-    //     </div>
-    //   </React.Fragment>
-    // );
-
     return (
-      <>
-        {user ? this.renderSignedInNavbar() : this.renderSignedOutNavbar()}
-
-        {/* only for private nav */}
-
-        {/* navbar menu_btn logo search_bar nav_signin_btn nav_register_btn for common */}
-        {/* nav_home menu_btn_home logo_home search_bar_home nav_signin_btn_home nav_register_btn_home for full width nav unregistered */}
-        {/* nav_public menu_btn_public logo_public search_bar_public nav_signin_btn_public nav_register_btn_public for half width nav unregistered */}
-        {/* nav_private menu_btn_private logo_private search_bar_private for half width nav registered */}
-
-        {/* if mentor only profile private_nav_btn1 style={display: none; visibility: hidden;} */}
-        {/* if mentee only profile private_nav_btn1 style={left: 67.19vw;} */}
-        {/* for unregustered users private_nav_btn1, private_nav_btn2 style={display: none; visibility: hidden;} */}
-      </>
+      <React.Fragment>
+        <div className={styles.keep_margin}>
+          {user ? this.renderSignedInNavbar() : this.renderSignedOutNavbar()}
+        </div>
+      </React.Fragment>
     );
   }
 
   renderSignedInNavbar() {
     return (
-      <div
-        className="row navbar
-    nav_home
-    nav_public
-    nav_private
-    v-align"
-      >
-        <button className="private_nav_btn1 btn-primary">Find Mentees</button>
-        <button className="private_nav_btn2 btn-primary">Find Mentors</button>
-        <img
-          src="../../assets/img/avatar.png"
-          alt="profile"
-          className="profile_icon
-          profile_icon_home
-          profile_icon_public
-          profile_icon_private"
-          onClick={this.handleProfileClick}
-        />
-      </div>
+      <Menu secondary>
+        <NavLink to='/'>
+          <div className={styles.fior_logo}>
+            <Image
+              src='../../assets/logo/logo.svg'
+              alt='Fior Logo'
+              size='small'
+            />
+          </div>
+        </NavLink>
+        <Menu.Menu position='right'>
+          <Menu.Item>Find Mentees</Menu.Item>
+          <Menu.Item>Find Mentors</Menu.Item>
+          <div className={styles.avatar}>
+            <Dropdown simple icon='user'>
+              <Dropdown.Menu>
+                <Dropdown.Item
+                  onClick={this.handleProfileClick}
+                  text='Proflie'
+                />
+                <Dropdown.Item
+                  onClick={this.handleSignOutClick}
+                  text='Sign Out'
+                />
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+        </Menu.Menu>
+      </Menu>
     );
   }
 
   renderSignedOutNavbar() {
     return (
-      <div className="row navbar nav_private v-align">
-        <input
-          type="text"
-          placeholder="Search"
-          className="search_bar
-          search_bar_home
-          search_bar_public
-          search_bar_private"
-        />
-
-        <button
-          className="nav_signin_btn nav_signin_btn_public btn-primary"
-          onClick={this.handleSignInClick}
-        >
-          Sign In
-        </button>
-
-        {/* <button className='nav_register_btn nav_register_btn_public secondary_btn'>
-          Register
-        </button> */}
-      </div>
+      <Menu secondary>
+        <NavLink to='/'>
+          <div className={styles.fior_logo}>
+            <Image
+              src='../../assets/logo/logo.svg'
+              alt='Fior Logo'
+              size='small'
+            />
+          </div>
+        </NavLink>
+        <Menu.Menu position='right'>
+          <Menu.Item onClick={this.handleSignInClick}>Sign In</Menu.Item>
+        </Menu.Menu>
+      </Menu>
     );
   }
 
@@ -106,24 +82,10 @@ class Navbar extends Component {
     this.props.history.push('/profile');
   };
 
-  handleAvatarMenuClick = (e, { value }) => {
+  handleSignOutClick = () => {
     const { history, signOut } = this.props;
-
-    console.log(value);
-
-    switch (value) {
-      case 0:
-        history.push('/profile');
-        break;
-
-      case 1:
-        signOut();
-        history.push('/');
-        break;
-
-      default:
-        break;
-    }
+    signOut();
+    history.push("/");
   };
 }
 
