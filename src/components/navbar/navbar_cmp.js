57c54eed8ca4bdb3b1464adde5ed844772f91b03
Sashika Nawarathne <hi@sashika.me>
2020-08-29 14:52:40 +0530

    Fix linting issues

--- src/components/navbar/navbar_cmp.js
+++ src/components/navbar/navbar_cmp.js
@@ -1,51 +1,58 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { NavLink } from "react-router-dom";
-import { signOut } from "../../store/actions/authActions";
-import { withRouter } from "react-router-dom";
-import { Menu, Image, Dropdown } from "semantic-ui-react";
-import styles from "../../styles/navbar.module.css";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { NavLink, withRouter } from 'react-router-dom';
+import { Menu, Image, Dropdown } from 'semantic-ui-react';
+import { signOut } from '../../store/actions/authActions';
+
+import styles from '../../styles/navbar.module.css';
 
 class Navbar extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
 
-  render() {
-    const { user } = this.props;
-
-    return (
-      <React.Fragment>
-        <div className={styles.keep_margin}>
-          {user ? this.renderSignedInNavbar() : this.renderSignedOutNavbar()}
-        </div>
-      </React.Fragment>
-    );
+    this.state = {};
   }
+
+  handleSignInClick = () => {
+    this.props.history.push('/login');
+  };
+
+  handleProfileClick = () => {
+    this.props.history.push('/profile');
+  };
+
+  handleSignOutClick = () => {
+    // eslint-disable-next-line no-shadow
+    const { history, signOut } = this.props;
+    signOut();
+    history.push('/');
+  };
 
   renderSignedInNavbar() {
     return (
       <Menu secondary>
-        <NavLink to='/'>
+        <NavLink to="/">
           <div className={styles.fior_logo}>
             <Image
-              src='../../assets/logo/logo.svg'
-              alt='Fior Logo'
-              size='small'
+              src="../../assets/logo/logo.svg"
+              alt="Fior Logo"
+              size="small"
             />
           </div>
         </NavLink>
-        <Menu.Menu position='right'>
+        <Menu.Menu position="right">
           <Menu.Item>Find Mentees</Menu.Item>
           <Menu.Item>Find Mentors</Menu.Item>
           <div className={styles.avatar}>
-            <Dropdown simple icon='user'>
+            <Dropdown simple icon="user">
               <Dropdown.Menu>
                 <Dropdown.Item
                   onClick={this.handleProfileClick}
-                  text='Proflie'
+                  text="Proflie"
                 />
                 <Dropdown.Item
                   onClick={this.handleSignOutClick}
-                  text='Sign Out'
+                  text="Sign Out"
                 />
               </Dropdown.Menu>
             </Dropdown>
@@ -58,43 +65,39 @@ class Navbar extends Component {
   renderSignedOutNavbar() {
     return (
       <Menu secondary>
-        <NavLink to='/'>
+        <NavLink to="/">
           <div className={styles.fior_logo}>
             <Image
-              src='../../assets/logo/logo.svg'
-              alt='Fior Logo'
-              size='small'
+              src="../../assets/logo/logo.svg"
+              alt="Fior Logo"
+              size="small"
             />
           </div>
         </NavLink>
-        <Menu.Menu position='right'>
+        <Menu.Menu position="right">
           <Menu.Item onClick={this.handleSignInClick}>Sign In</Menu.Item>
         </Menu.Menu>
       </Menu>
     );
   }
 
-  handleSignInClick = () => {
-    this.props.history.push('/login');
-  };
+  render() {
+    const { user } = this.props;
 
-  handleProfileClick = () => {
-    this.props.history.push('/profile');
-  };
-
-  handleSignOutClick = () => {
-    const { history, signOut } = this.props;
-    signOut();
-    history.push("/");
-  };
+    return (
+      <>
+        <div className={styles.keep_margin}>
+          {user ? this.renderSignedInNavbar() : this.renderSignedOutNavbar()}
+        </div>
+      </>
+    );
+  }
 }
 
 const mapStateToProps = (state) => ({
   user: state.auth.currentUser,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  signOut: () => dispatch(signOut()),
-});
+const mapDispatchToProps = { signOut };
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Navbar));
