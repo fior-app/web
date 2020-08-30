3c5db2e50a600b0f54301b45479250ce0023dbc1
Sashika Nawarathne <hi@sashika.me>
2020-08-30 13:00:09 +0530

    Update login ui

--- src/components/sidenav/sidenav_cmp.js
+++ src/components/sidenav/sidenav_cmp.js
@@ -9,51 +9,9 @@ const SideNav = (user) => (
         <img src="../../assets/logo/logo.svg" alt="Fior Logo" />
       </NavLink>
 
-      <div className="sidenav_items">
-        <NavLink to="/">
-          <button type="button" className="btn-primary">Home</button>
-        </NavLink>
-        {user && (
-          <NavLink to="/mentorspaces">
-            <button type="button" className="btn-primary">Mentorspace</button>
-          </NavLink>
-        )}
-        {user && (
-          <NavLink to="/orgs">
-            <button type="button" className="btn-primary">Organizations</button>
-          </NavLink>
-        )}
-        <NavLink to="question-forum">
-          <button type="button" className="btn-primary">Question Forum</button>
-        </NavLink>
-        <NavLink to="/blog">
-          <button type="button" className="btn-primary">Blog</button>
-        </NavLink>
-        {user && (
-          <NavLink to="/users">
-            <button type="button" className="btn-primary">Users</button>
-          </NavLink>
-        )}
-        {user && (
-          <NavLink to="/notifications">
-            <button type="button" className="btn-primary">Notifications</button>
-          </NavLink>
-        )}
-        {user && (
-          <NavLink to="/settings">
-            <button type="button" className="btn-primary">Settings</button>
-          </NavLink>
-        )}
-        <NavLink to="/pricing">
-          <button type="button" className="btn-primary ">Pricing Plan</button>
-        </NavLink>
-        <NavLink to="about">
-          <button type="button" className="btn-primary ">About Us</button>
-        </NavLink>
-      </div>
-    </div>
-  </>
-);
+    return (
+      <>
+        <div className="sidenav_container">
 
 const mapStateToProps = (state) => ({
   user: state.auth.currentUser,
