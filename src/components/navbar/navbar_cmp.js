af529bc33c1154fbc4a92636ae62000db9332c81
UdeshUK <mail@udesh.xyz>
2020-09-03 14:33:01 +0530

    Navbar fix

--- src/components/navbar/navbar_cmp.js
+++ src/components/navbar/navbar_cmp.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { NavLink, withRouter } from 'react-router-dom';
-import { Menu, Image, Dropdown } from 'semantic-ui-react';
-import { signOut } from '../../store/actions/authActions';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {NavLink, withRouter} from 'react-router-dom';
+import {Menu, Image, Dropdown} from 'semantic-ui-react';
+import {signOut} from '../../store/actions/authActions';
 
 import styles from '../../styles/navbar.module.css';
 
@@ -15,68 +15,52 @@ class Navbar extends Component {
 
   handleSignOutClick = () => {
     // eslint-disable-next-line no-shadow
-    const { history, signOut } = this.props;
+    const {history, signOut} = this.props;
     signOut();
     history.push('/');
   };
 
-  renderSignedInNavbar() {
-    return (
-      <Menu secondary>
-        <NavLink to="/">
-          <div className={styles.fior_logo}>
-            <Image
-              src="../../assets/logo/logo.svg"
-              alt="Fior Logo"
-              size="small"
-            />
-          </div>
-        </NavLink>
-        <Menu.Menu position="right">
-          <Menu.Item>Find Mentees</Menu.Item>
-          <Menu.Item>Find Mentors</Menu.Item>
-          <div className={styles.avatar}>
-            <Dropdown icon="user">
-              <Dropdown.Menu>
-                <Dropdown.Item
-                  onClick={() => this.props.history.push('/profile')}
-                  text="Profile"
-                />
-                <Dropdown.Item
-                  onClick={() => this.props.history.push('/mentorspaces')}
-                  text="Mentorspaces"
-                />
-                <Dropdown.Item
-                  onClick={this.handleSignOutClick}
-                  text="Sign Out"
-                />
-              </Dropdown.Menu>
-            </Dropdown>
-          </div>
-        </Menu.Menu>
-      </Menu>
-    );
-  }
+  render() {
+    const {user} = this.props;
 
     return (
-      <Menu secondary>
-        <NavLink to="/">
-          <div className={styles.fior_logo}>
-            <Image
-              src="../../assets/logo/logo.svg"
-              alt="Fior Logo"
-              size="small"
-            />
-          </div>
-        </NavLink>
-        <Menu.Menu position="left">
-          <Menu.Item as={NavLink} name="blog" to="/blog">Blog</Menu.Item>
-          <Menu.Item as={NavLink} name="forum" to="/forum">Forum</Menu.Item>
-        </Menu.Menu>
-        <Menu.Menu position="right">
-          <Menu.Item onClick={() => this.props.history.push('/login')}>Sign In</Menu.Item>
-        </Menu.Menu>
-      </Menu>
+      <div className={styles.keep_margin}>
+        <Menu secondary>
+          <NavLink to="/">
+            <div className={styles.fior_logo}>
+              <Image
+                src="../../assets/logo/logo.svg"
+                alt="Fior Logo"
+                size="small"
+              />
+            </div>
+          </NavLink>
+          <Menu.Menu position="left">
+            <Menu.Item as={NavLink} name="blog" to="/blog">Blog</Menu.Item>
+            <Menu.Item as={NavLink} name="forum" to="/forum">Forum</Menu.Item>
+          </Menu.Menu>
+          <Menu.Menu position="right">
+            {user ? (
+              <>
+                <Menu.Item>Find Mentees</Menu.Item>
+                <Menu.Item>Find Mentors</Menu.Item>
+                <div className={styles.avatar}>
+                  <Dropdown simple icon="user">
+                    <Dropdown.Menu>
+                      <Dropdown.Item as={NavLink} text="Profile" to="/profile"/>
+                      <Dropdown.Item as={NavLink} text="Mentorspaces" to="/mentorspaces"/>
+                      <Dropdown.Item
+                        onClick={this.handleSignOutClick}
+                        text="Sign Out"
+                      />
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </div>
+              </>
+            ) : <Menu.Item as={NavLink} text="Login" to="/login">Sign In</Menu.Item>}
+          </Menu.Menu>
+        </Menu>
+      </div>
     );
   }
 }
@@ -85,6 +69,6 @@ const mapStateToProps = (state) => ({
   user: state.auth.currentUser,
 });
 
-const mapDispatchToProps = { signOut };
+const mapDispatchToProps = {signOut};
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Navbar));
