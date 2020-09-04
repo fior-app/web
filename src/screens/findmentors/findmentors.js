202a583e7d1d327cd047f9568b8148ba1cd7d473
UdeshUK <mail@udesh.xyz>
2020-09-05 03:07:04 +0530

    Implemented the mentor search header

--- src/screens/findmentors/findmentors.js
+++ src/screens/findmentors/findmentors.js
@@ -1,11 +1,38 @@
 import React from "react";
+import { Container, Header, Search } from "semantic-ui-react";
+import _ from "lodash";
 
-const FindMentorsScreen = () => {
+const FindMentorsScreen = ({
+  mentorSearch = {
+    isSearching: false,
+    results: null,
+    value: ''
+  }
+}) => {
+
+  const suggestionSelected = (e, { result }) => {
+    console.log(result);
+  }
+
+  const handleOnSearch = (e, { value }) => {
+    console.log(value)
+  }
 
   return (
-    <div>
-      Find Mentors
-    </div>
+    <Container>
+      <Header as={'h1'}>Find Mentors</Header>
+      <Header sub>“It’s been true in my life that when I’ve needed a mentor, the right person shows up.” – Ken Blanchard</Header>
+      <Search
+        loading={mentorSearch.searching}
+        onResultSelect={suggestionSelected}
+        onSearchChange={_.debounce(handleOnSearch, 500, {
+          leading: true,
+        })}
+        results={mentorSearch.results}
+        value={mentorSearch.value}
+        fluid
+      />
+    </Container>
   );
 }
 
