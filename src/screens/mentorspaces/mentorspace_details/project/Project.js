f174d3de93c83eb2beb94cdf5dce2586d5ecafe1
hirantha <mail@hirantha.xyz>
2020-09-06 22:21:26 +0530

    bug fix

--- src/screens/mentorspaces/mentorspace_details/project/Project.js
+++ src/screens/mentorspaces/mentorspace_details/project/Project.js
@@ -44,7 +44,7 @@ const Project = ({ projects }) => {
         </Grid.Row>
       </Grid>
       <Divider />
-      {projects ? (
+      {projects && projects[mentorspaceId] ? (
         <ProjectComponent project={projects[mentorspaceId]} />
       ) : (
         <NoProject />
