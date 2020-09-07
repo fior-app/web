eee90aa271a2db049c18cfa7e1e48b5dfed8a712
hirantha <mail@hirantha.xyz>
2020-09-07 11:34:08 +0530

    github projects added to project widget

--- src/screens/mentorspaces/mentorspace_details/project/ProjectWidget.js
+++ src/screens/mentorspaces/mentorspace_details/project/ProjectWidget.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
+import { NavLink, Link } from 'react-router-dom';
+import { Card, Divider, Button, Header } from 'semantic-ui-react';
 
 import * as styles from '../../../../styles/mentorspace-project.module.css';
-import { Card, Divider, Button, Header } from 'semantic-ui-react';
-import { NavLink } from 'react-router-dom';
 
 export class ProjectWidget extends Component {
   constructor() {
@@ -13,18 +13,25 @@ export class ProjectWidget extends Component {
     this.state = {
       title: '',
       description: '',
+      githubLinks: [],
+      projectId: null,
     };
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.projects) {
       const project = nextProps.projects[this.props.mentorspaceId];
-      this.setState({ title: project.title, description: project.description });
+      this.setState({
+        title: project.title,
+        description: project.description,
+        githubLinks: project.githubLinks,
+        projectId: project.projectId,
+      });
     }
   }
 
   render() {
-    const { title, description } = this.state;
+    const { title, description, githubLinks, projectId } = this.state;
     return (
       <Card>
         <Card.Content>
@@ -36,6 +43,20 @@ export class ProjectWidget extends Component {
                 <Header as="h3">{title}</Header>
               </Card.Description>
               <Card.Meta>{description}</Card.Meta>
+              {githubLinks &&
+                githubLinks.map((link, index) => {
+                  return (
+                    <Card
+                      as={Link}
+                      to={`/mentorspaces/${projectId}/project`}
+                      key={index}
+                    >
+                      <Card.Content>
+                        {`${link.owner}/${link.repository}`}
+                      </Card.Content>
+                    </Card>
+                  );
+                })}
             </>
           ) : (
             <NavLink
