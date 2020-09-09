57939e6319e7f8899424651592d7760f73087dfa
hirantha <mail@hirantha.xyz>
2020-09-10 02:13:30 +0530

    github link card

--- src/screens/mentorspaces/mentorspace_details/project/GithubRepositories.js
+++ src/screens/mentorspaces/mentorspace_details/project/GithubRepositories.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { compose } from 'redux';
 import { Divider } from 'semantic-ui-react';
 import { GithubLink } from './GithubLink';
+import { firestoreConnect } from 'react-redux-firebase';
 
 import * as styles from './../../../../styles/mentorspace-project.module.css';
 import AddRepository from './AddRepository';
 
 export class GithubRepositories extends Component {
   render() {
+    const { mentorspaceId } = this.props;
+
     return (
       <div className={styles.github_card}>
         <div className={styles.header}>
           <span>Github Repositories</span>
-          <AddRepository projectId={this.props.mentorspaceId} />
+          <AddRepository projectId={mentorspaceId} />
         </div>
         <Divider />
-        <GithubLink link={'sexy'} />
+        {this.props.projects[mentorspaceId].githubLinks.map((link, index) => (
+          <GithubLink link={link} key={index} />
+        ))}
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({
+  projects: state.firestore.data.projects,
+});
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = (dispatch) => ({});
 
-export default connect(mapStateToProps, mapDispatchToProps)(GithubRepositories);
+export default compose(
+  firestoreConnect((props) => [
+    {
+      collection: 'projects',
+      where: ['projectId', '==', props.mentorspaceId],
+    },
+  ]),
+  connect(mapStateToProps, mapDispatchToProps)
+)(GithubRepositories);
