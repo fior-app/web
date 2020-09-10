de57e6bc9cb3354e40608509406db01995b3b109
hirantha <mail@hirantha.xyz>
2020-09-10 14:09:18 +0530

    issues added for github project

--- src/screens/mentorspaces/mentorspace_details/project/GithubLink.js
+++ src/screens/mentorspaces/mentorspace_details/project/GithubLink.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Image, Divider, Card, Icon } from 'semantic-ui-react';
+import { Image, Divider, Card, Icon, Header } from 'semantic-ui-react';
 import Axios from 'axios';
 
 import * as styles from './../../../../styles/mentorspace-project.module.css';
-import { Link } from 'react-router-dom';
 
 export class GithubLink extends Component {
   constructor(props) {
@@ -16,9 +15,13 @@ export class GithubLink extends Component {
       contributors: null,
       contributorsLoading: null,
       contributorsError: null,
+      issues: null,
+      issuesLoading: null,
+      issuesError: null,
     };
     this.loadData(props.link);
     this.loadContributors(props.link);
+    this.loadIssues(props.link);
   }
 
   loadData = (link) => {
@@ -49,6 +52,22 @@ export class GithubLink extends Component {
       );
   };
 
+  loadIssues = (link) => {
+    Axios.get(
+      `https://api.github.com/repos/${link.owner}/${link.repository}/issues`,
+      {
+        headers: { Authorization: '' },
+      }
+    )
+      .then((res) => this.setState({ issues: res.data, issuesLoading: false }))
+      .catch((error) =>
+        this.setState({
+          issuesError: error.message,
+          issuesLoading: false,
+        })
+      );
+  };
+
   render() {
     const {
       loading,
@@ -57,7 +76,13 @@ export class GithubLink extends Component {
       contributors,
       contributorsError,
       contributorsLoading,
+      issues,
+      issuesError,
+      issuesLoading,
     } = this.state;
+
+    console.log(issues);
+
     return (
       <div className={styles.github_link_card}>
         {loading ? <div>Loading</div> : null}
@@ -74,13 +99,29 @@ export class GithubLink extends Component {
             </div>
             <Divider />
             <div className={styles.description}>{data.description}</div>
-            <a href={data.html_url + '/issues'}>
-              <div className={styles.open_issues}>
-                {data.open_issues} open issues found
-              </div>
-            </a>
+            {issues && issues.length !== 0 && (
+              <>
+                <Header as="h3">Issues</Header>
+                <Divider />
+              </>
+            )}
+            {issues && (
+              <Card.Group
+                items={issues
+                  .filter((issue) => issue.pull_request === undefined)
+                  .map((issue) => {
+                    return {
+                      header: issue.title,
+                      description: issue.body,
+                      meta: `${issue.user.login} opened this issue`,
+                      href: issue.html_url,
+                    };
+                  })}
+              />
+            )}
           </div>
         ) : null}
+        <Header as="h3">Contributors</Header>
         <Divider />
         {contributorsLoading ? <div>Loading Contributors</div> : null}
         {contributorsError ? <div>{error}</div> : null}
@@ -88,7 +129,7 @@ export class GithubLink extends Component {
         {contributors ? (
           <Card.Group>
             {contributors.map((contributor) => (
-              <Card>
+              <Card key={contributor.login}>
                 <Image src={contributor.avatar_url} wrapped ui={false} />
                 <Card.Content>
                   <Card.Header>{contributor.login}</Card.Header>
