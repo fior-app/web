d80c697c8d51440ad46dd2b4ccea05a6530a4c7d
hirantha <mail@hirantha.xyz>
2020-10-03 14:29:28 +0530

    project removed from mentorspace create dialog

--- src/screens/mentorspaces/create/create_mentorspace.js
+++ src/screens/mentorspaces/create/create_mentorspace.js
@@ -1,64 +1,84 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Form, Button } from 'semantic-ui-react';
 
-import { createMentorspace } from "../../../store/actions/mentorspaceActions";
-import MentorspaceDetails from "./mentorspace_details";
-import ProjectDetails from "./project_details";
+import { createMentorspace } from '../../../store/actions/mentorspaceActions';
+import styles from '../../../styles/mentorspaces.module.css';
 
 class CreateMentorspace extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      group: {},
-      project: {},
-      active: 0,
+      name: '',
+      description: '',
+      icon: '',
     };
   }
 
-  handleCreateMentorspace = async (type, value) => {
+  handleCreateMentorspace = () => {
     const { createSpace, closeModal } = this.props;
-
-    await this.setState({ [type]: value });
     createSpace(this.state, closeModal);
   };
 
-  handleNext = (type, value) => {
-    this.setState((state) => ({
-      active: state.active + 1,
-      [type]: value,
-    }));
-  };
-
-  handleBack = (type, value) => {
-    this.setState((state) => ({
-      active: state.active - 1,
-      [type]: value,
-    }));
+  handleOnChangeInput = (e) => {
+    this.setState({ [e.target.id]: e.target.value });
   };
 
   render() {
-    const { active, group, project } = this.state;
+    const { name, description, icon } = this.state;
     const { error, loading } = this.props;
 
-    switch (active) {
-      case 0:
-        return (
-          <MentorspaceDetails handleNext={this.handleNext} group={group} />
-        );
-      case 1:
-        return (
-          <ProjectDetails
-            handleBack={this.handleBack}
-            error={error}
-            loading={loading}
-            handleCreateMentorspace={this.handleCreateMentorspace}
-            project={project}
-          />
-        );
-      default:
-        return <div />;
-    }
+    return (
+      <div className={styles.modal_container}>
+        <h3>Create your awesome mentorspace</h3>
+
+        <Form>
+          <Form.Field>
+            <input
+              type="text"
+              id="name"
+              placeholder="Name"
+              value={name}
+              onChange={this.handleOnChangeInput}
+            />
+          </Form.Field>
+          <Form.Field>
+            <input
+              type="text"
+              id="description"
+              placeholder="Description"
+              value={description}
+              onChange={this.handleOnChangeInput}
+            />
+          </Form.Field>
+          <Form.Field>
+            <input
+              type="text"
+              id="icon"
+              placeholder="Icon"
+              value={icon}
+              onChange={this.handleOnChangeInput}
+            />
+          </Form.Field>
+
+          {error && JSON.stringify(error)}
+          {loading ? (
+            <div className={styles.information}>Creating mentorspace...</div>
+          ) : null}
+
+          <div className={styles.row}>
+            <Button
+              className="btn-alternate"
+              disabled={loading}
+              onClick={this.handleCreateMentorspace}
+            >
+              Create
+            </Button>
+          </div>
+        </Form>
+      </div>
+    );
   }
 }
 
