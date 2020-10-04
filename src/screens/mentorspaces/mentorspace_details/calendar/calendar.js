ecb7f145c9eaa110a171c6fcb15a093196bfae4c
Sashika Nawarathne <hi@sashika.me>
2020-10-04 21:48:01 +0530

    Implement event add on timeline

--- src/screens/mentorspaces/mentorspace_details/calendar/calendar.js
+++ src/screens/mentorspaces/mentorspace_details/calendar/calendar.js
@@ -1,35 +1,103 @@
-import React from 'react';
-import { Divider, Header } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import {
+  Button,
+  Divider, Header, Icon, Modal,
+} from 'semantic-ui-react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
 import { compose } from 'redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { connect } from 'react-redux';
-import { object } from 'prop-types';
+import { useParams } from 'react-router-dom';
+import styles from '../../../../styles/calendar.module.css';
+import AddEditMilestone from '../milestones/add_edit_milestone';
+import AddEditMeetings from '../meetings/add_edit_meetings';
 
 const Calendar = ({ milestones, meetings }) => {
+  const initialState = {
+    selectedDate: '',
+  };
+
+  const [state, setState] = useState(initialState);
+
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const { mentorspaceId } = useParams();
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  const handleCellOnClick = (e) => {
+    setState({ selectedDate: e.dateStr });
+    openModal();
+  };
+
   if (milestones && meetings) {
     let events = Object.values(milestones).map((milestone) => ({
       title: milestone.title,
       date: milestone.due,
-      color: 'teal',
+      color: '#02c39a',
     }));
 
     events = events.concat(Object.values(meetings).map((meeting) => ({
       title: meeting.title,
       date: meeting.on,
-      color: 'blue',
+      color: '#2185d0',
     })));
 
     return (
       <>
         <Header as="h2">Timeline</Header>
         <Divider />
+        <div className={styles.legend}>
+          <div className={styles.row}>
+            <div className={styles.milestones_block} />
+            <div className={styles.spacer_sm} />
+            Milestones
+          </div>
+          <div className={styles.row}>
+            <div className={styles.meetings_block} />
+            <div className={styles.spacer_sm} />
+            Meetings
+          </div>
+        </div>
         <FullCalendar
-          plugins={[dayGridPlugin]}
+          plugins={[dayGridPlugin, interactionPlugin]}
           initialView="dayGridMonth"
           events={events}
+          dateClick={handleCellOnClick}
         />
+        <Modal
+          basic
+          size="small"
+          onClose={closeModal}
+          open={isModalOpen}
+        >
+          <Modal.Header>
+            What do you want to add on
+            {' '}
+            {state.selectedDate}
+          </Modal.Header>
+          <Modal.Content>
+            <AddEditMilestone
+              mentorspaceId={mentorspaceId}
+              date={state.selectedDate}
+              trigger={(open) => (
+                <Button onClick={open}>
+                  <Icon name="plus" />
+                  Milestone
+                </Button>
+              )}
+            />
+            <AddEditMeetings mentorspaceId={mentorspaceId} date={state.selectedDate} />
+          </Modal.Content>
+        </Modal>
       </>
     );
   }
