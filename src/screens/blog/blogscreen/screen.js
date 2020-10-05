4ba85e2532d501accaf375e58e4b63cb9e914103
Sachie <sachiejayen@gmail.com>
2020-10-05 12:59:48 +0530

    screen update

---
+++ src/screens/blog/blogscreen/screen.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { Link, NavLink } from 'react-router-dom';
+
+
+const BlogScreen = ({
+  posts,
+  skills,
+  dispatchGetPosts,
+}) => {
+  const initialState = {
+    activePage: 1,
+    limit: 5,
+  };
+
+  const [blogState, setBlogState] = useState(initialState);
+
+  
+
+  useEffect(() => {
+    dispatchGetPosts();
+  }, [dispatchGetPosts]);
+
+  const handlePaginationChange = (e, { activePage }) => setBlogState((state) => ({
+    ...state,
+    activePage,
+  }));
+
+  
+
+  return (
+    <Container>
+      <Grid columns="equal">
+        <Grid.Column>
+          <Grid.Row>
+            {posts.slice(getPageStart(), getPageEnd())
+              .map((post) => (
+                <Link to={`/blog/${post.id}`} key={post.id}>
+                  <Post post={post} />
+                </Link>
+              ))}
+          </Grid.Row>
+          <Grid.Row>
+            <Pagination
+              activePage={blogState.activePage}
+              firstItem={null}
+              lastItem={null}
+              onPageChange={handlePaginationChange}
+              totalPages={posts.length / 5}
+            />
+          </Grid.Row>
+        </Grid.Column>
+        <Grid.Column width={4}>
+          <Button as={NavLink} to="/blog/edit" primary>Create Post</Button>
+          <Categories categories={skills} />
+          <RecentPosts posts={recentPosts} />
+        </Grid.Column>
+      </Grid>
+    </Container>
+  );
+};
+
+
+
+const mapDispatchToProps = (dispatch) => ({
+  dispatchGetPosts: () => dispatch(getPosts()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(BlogScreen);
