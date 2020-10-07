db7f95d99cc8085c14913e9f07732e6c8537da10
hirantha <mail@hirantha.xyz>
2020-10-07 12:19:32 +0530

    contributors for github repository

--- src/screens/mentorspaces/mentorspace_details/project/GithubLink.js
+++ src/screens/mentorspaces/mentorspace_details/project/GithubLink.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Image, Divider } from 'semantic-ui-react';
+import { Image, Divider, Card, Icon } from 'semantic-ui-react';
 import Axios from 'axios';
 
 import * as styles from './../../../../styles/mentorspace-project.module.css';
@@ -13,8 +13,12 @@ export class GithubLink extends Component {
       loading: true,
       data: null,
       error: null,
+      contributors: null,
+      contributorsLoading: null,
+      contributorsError: null,
     };
     this.loadData(props.link);
+    this.loadContributors(props.link);
   }
 
   loadData = (link) => {
@@ -27,8 +31,33 @@ export class GithubLink extends Component {
       );
   };
 
+  loadContributors = (link) => {
+    Axios.get(
+      `https://api.github.com/repos/${link.owner}/${link.repository}/contributors`,
+      {
+        headers: { Authorization: '' },
+      }
+    )
+      .then((res) =>
+        this.setState({ contributors: res.data, contributorsLoading: false })
+      )
+      .catch((error) =>
+        this.setState({
+          contributorsError: error.message,
+          contributorsLoading: false,
+        })
+      );
+  };
+
   render() {
-    const { loading, error, data } = this.state;
+    const {
+      loading,
+      error,
+      data,
+      contributors,
+      contributorsError,
+      contributorsLoading,
+    } = this.state;
     return (
       <div className={styles.github_link_card}>
         {loading ? <div>Loading</div> : null}
@@ -51,6 +80,34 @@ export class GithubLink extends Component {
               </div>
             </a>
           </div>
+        ) : null}
+        <Divider />
+        {contributorsLoading ? <div>Loading Contributors</div> : null}
+        {contributorsError ? <div>{error}</div> : null}
+
+        {contributors ? (
+          <Card.Group>
+            {contributors.map((contributor) => (
+              <Card>
+                <Image src={contributor.avatar_url} wrapped ui={false} />
+                <Card.Content>
+                  <Card.Header>{contributor.login}</Card.Header>
+                  {/* <Card.Meta>
+                  <span className='date'>Joined in 2015</span>
+                </Card.Meta>
+                <Card.Description>
+                  Matthew is a musician living in Nashville.
+                </Card.Description> */}
+                </Card.Content>
+                <Card.Content extra>
+                  <a>
+                    <Icon name="user" />
+                    {contributor.contributions} Contributions to repository
+                  </a>
+                </Card.Content>
+              </Card>
+            ))}
+          </Card.Group>
         ) : null}
       </div>
     );
