9563276e087002f6aad80e9dc46fa903573b4427
Sashika Nawarathne <hi@sashika.me>
2020-10-08 10:03:13 +0530

    Update animation

--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { Image } from 'semantic-ui-react';
 
 const BaseLoading = () => (
-  <div style={{ marginTop: '24rem' }}>
-    <div style={{
-      display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center',
-    }}
-    >
-      <Image src="assets/svg/loading.svg" size="small" />
-      <Image src="assets/logo/logo_name.png" size="small" wrapped />
+  <>
+    <div style={{ marginTop: '26rem' }}>
+      <div style={{
+        display: 'flex', flexDirection: 'row', justifyContent: 'center',
+      }}
+      >
+        <Image src="assets/svg/loading.svg" size="tiny" />
+      </div>
     </div>
-  </div>
+  </>
 );
 
 export default BaseLoading;
