13992f6e7d31e3aa27b86b1173f52042f12acc7c
UdeshUK <mail@udesh.xyz>
2020-10-08 10:15:37 +0530

    Question bug fix

--- src/screens/question/view_question.js
+++ src/screens/question/view_question.js
@@ -14,130 +14,134 @@ import loading from '../blog/edit/edit_blog_post';
 import styles from '../../styles/question.module.css';
 
 class ViewQuestion extends Component {
-    state = {
+  state = {
+    description: '',
+  };
+
+  componentDidMount() {
+    this.props.getQuestion(this.props.match.params.questionId);
+    this.props.getAnswer(this.props.match.params.questionId);
+  }
+
+  onSuccessCallback = () => {
+    this.setState({
       description: '',
-    };
+    });
+  }
 
-    componentDidMount() {
-      this.props.getQuestion(this.props.match.params.questionId);
-      this.props.getAnswer(this.props.match.params.questionId);
-    }
+  // componentWillUnmount(){
+  //     this.props.clearState();
+  // }
 
-    onSuccessCallback = () => {
-      this.setState({
-        description: '',
-      });
-    }
+  render() {
+    const { questionId } = this.props.match.params;
+    const { question, answers, createAnswer, user } = this.props;
+    const { loading, successMsg } = this.props;
+    const { description } = this.state;
 
-    // componentWillUnmount(){
-    //     this.props.clearState();
-    // }
+    console.log(answers);
 
-    render() {
-      const { questionId } = this.props.match.params;
-      const { question, answers, createAnswer } = this.props;
-      const { loading, successMsg } = this.props;
-      const { description } = this.state;
-
-      console.log(answers);
-
-      return (
-        <Container>
-          <Link to="/forum">← Back to Question Forum</Link>
-          {' '}
-          <br />
-          <br />
-          <div>
-            <Card fluid color="green">
-              <Card.Content>
-                <div className={styles.ques_label}>
-                  <Label as="a" color="red" tag>
-                    Question
-                  </Label>
-                </div>
-                <div className={styles.ques_content_header}>
-                  <Card.Header>
-                    {question.question && question.question.title}
-                  </Card.Header>
-                </div>
-                <div className={styles.ques_content_description}>
-                  <Card.Description>
-                    {question.question && question.question.description}
-                  </Card.Description>
-                </div>
-                <div className={styles.answer_date}>
-                  <Card.Meta textAlign="right">
-                    3rd Oct 2020
-                  </Card.Meta>
-                </div>
-              </Card.Content>
-            </Card>
-            <h3>Answers</h3>
-            {answers.answers && answers.answers.map((answer) => (
-              <div className="answer_box">
-                <Card fluid>
-                  <Card.Content>
-                    <Card.Description>
-                      {answer.description}
-                    </Card.Description>
-                    <div className={styles.answer_date}>
-                      <Card.Meta textAlign="right">3rd Oct 2020</Card.Meta>
-                    </div>
-                  </Card.Content>
-                </Card>
-                {' '}
-                <br />
+    return (
+      <Container>
+        <Link to="/forum">← Back to Question Forum</Link>
+        {' '}
+        <br/>
+        <br/>
+        <div>
+          <Card fluid color="green">
+            <Card.Content>
+              <div className={styles.ques_label}>
+                <Label as="a" color="red" tag>
+                  Question
+                </Label>
               </div>
-            ))}
-            <div>
-              {this.props.ansAnswer && this.props.ansAnswer.description}
+              <div className={styles.ques_content_header}>
+                <Card.Header>
+                  {question.question && question.question.title}
+                </Card.Header>
+              </div>
+              <div className={styles.ques_content_description}>
+                <Card.Description>
+                  {question.question && question.question.description}
+                </Card.Description>
+              </div>
+              <div className={styles.answer_date}>
+                <Card.Meta textAlign="right">
+                  3rd Oct 2020
+                </Card.Meta>
+              </div>
+            </Card.Content>
+          </Card>
+          <h3>Answers</h3>
+          {answers.answers && answers.answers.map((answer) => (
+            <div className="answer_box">
+              <Card fluid>
+                <Card.Content>
+                  <Card.Description>
+                    {answer.description}
+                  </Card.Description>
+                  <div className={styles.answer_date}>
+                    <Card.Meta textAlign="right">3rd Oct 2020</Card.Meta>
+                  </div>
+                </Card.Content>
+              </Card>
+              {' '}
+              <br/>
             </div>
-            <h3>Answer here</h3>
-            <Form onSubmit={this.handleSubmit}>
-              <Form.Field>
-                <Form.TextArea
-                  id="description"
-                  className="answer"
-                  type="textarea"
-                  placeholder="Answer"
-                  value={description}
-                  onChange={this.handleChange}
-                />
-              </Form.Field>
-              <Form.Field>
-                {answers.loading ? 'Posted Successfully!' : null}
-                <div className={styles.post_row}>
-                  <Button
-                    type="submit"
-                    disabled={loading}
-                    loading={loading}
-                    color="teal"
-                  >
-                    Post
-                  </Button>
-                </div>
-              </Form.Field>
-            </Form>
+          ))}
+          <div>
+            {this.props.ansAnswer && this.props.ansAnswer.description}
           </div>
-        </Container>
-      );
-    }
+          {user && (
+            <>
+              <h3>Answer here</h3>
+              <Form onSubmit={this.handleSubmit}>
+                <Form.Field>
+                  <Form.TextArea
+                    id="description"
+                    className="answer"
+                    type="textarea"
+                    placeholder="Answer"
+                    value={description}
+                    onChange={this.handleChange}
+                  />
+                </Form.Field>
+                <Form.Field>
+                  {answers.loading ? 'Posted Successfully!' : null}
+                  <div className={styles.post_row}>
+                    <Button
+                      type="submit"
+                      disabled={loading}
+                      loading={loading}
+                      color="teal"
+                    >
+                      Post
+                    </Button>
+                  </div>
+                </Form.Field>
+              </Form>
+            </>
+          )}
+        </div>
+      </Container>
+    );
+  }
 
-    fetchQuestion = () => {
+  fetchQuestion = () => {
 
-    }
+  }
 
-    handleSubmit = (e) => {
-      const { answer } = this.state;
-      e.preventDefault();
-      this.props.createAnswer(this.state, this.props.match.params.questionId, this.onSuccessCallback);
-    };
+  handleSubmit = (e) => {
+    const { answer } = this.state;
+    e.preventDefault();
+    this.props.createAnswer(this.state, this.props.match.params.questionId, this.onSuccessCallback);
+  };
 
-    handleChange = (e) => {
-      this.setState({
-        [e.target.id]: e.target.value,
-      });
-    };
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
 }
 
 const mapStateToProps = (state) => {
@@ -159,6 +163,7 @@ const mapStateToProps = (state) => {
       error: state.question.createAnswer.error,
       successMsg: state.question.successMsg,
     },
+    user: state.auth.currentUser,
   };
 };
 
