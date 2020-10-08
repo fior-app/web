ca474e442cfc3e90cfbfaa366fdfedffe14fb994
Prabhani Nanayakkara <prabhaninanayakkara1907@gmail.com>
2020-10-08 09:44:32 +0530

    Update pricing plan

--- src/screens/pricing/pricing_screen.js
+++ src/screens/pricing/pricing_screen.js
@@ -1,27 +1,28 @@
-import React from 'react';
+import React from "react";
+import styles from "./pricing.module.css";
 
 const PricingScreen = () => (
-  <div className="landing_pricing_plan_bg">
-    <div className="landing_pricing_plan">
-      <h3 className="landing_pricing_main_h3">Pricing Plan</h3>
-      <p className="landing_pricing_main_p">
+  <div className={styles.pricing_plan_bg}>
+    <div className={styles.pricing_plan}>
+      <h3 className={styles.pricing_main_h3}>Pricing Plan</h3>
+      <p className={styles.pricing_main_p}>
         Unlock many more features and resources
       </p>
 
       {/* Pricing Plan Packages */}
-      <div className="landing_pricing_packages row">
+      <div className={styles.pricing_packages}>
         {/* Free Package */}
-        <div className="landing_package1">
+        <div className={styles.pricing_package1}>
           <h4>Free</h4>
 
-          <div className="landing_pricing_content1">
-            <div className="landing_pricing_description1 row">
+          <div className={styles.pricing_content1}>
+            <div className={styles.pricing_description1}>
               <img src="../../img/check.svg" alt="Check Mark" />
-              <p className="landing_pricing_point">Create 2 groups</p>
+              <p className={styles.pricing_point}>Create 2 groups</p>
             </div>
-            <div className="landing_pricing_description row">
+            <div className={styles.pricing_description}>
               <img src="../../assets/img/check.svg" alt="Check Mark" />
-              <p className="landing_pricing_point">
+              <p className={styles.pricing_point}>
                 Add maximum 5 members per group
               </p>
             </div>
@@ -30,76 +31,82 @@ const PricingScreen = () => (
         {/* End of Free Package */}
 
         {/* Standard Package */}
-        <div className="landing_package2">
+        <div className={styles.pricing_package2}>
           <h4>Standard</h4>
-          <p className="landing_price1">
+          <p className={styles.price1}>
             <span>$6</span>
             <br />
             (One-time payment)
           </p>
 
-          <div className="landing_pricing_content2">
-            <div className="landing_pricing_description1 row">
+          <div className={styles.pricing_content2}>
+            <div className={styles.pricing_description1}>
               <img src="../../assets/img/check.svg" alt="Check Mark" />
-              <p className="landing_pricing_point">Create 10 groups</p>
+              <p className={styles.pricing_point}>Create 10 groups</p>
             </div>
-            <div className="landing_pricing_description row">
+            <div className={styles.pricing_description}>
               <img src="../../assets/img/check.svg" alt="Check Mark" />
-              <p className="landing_pricing_point">
+              <p className={styles.pricing_point}>
                 Add maximum 10 members per group
               </p>
             </div>
           </div>
 
-          <button type="button" className="secondary_btn">Choose Plan</button>
+          {/* <button type="button" className="secondary_btn">
+            Choose Plan
+          </button> */}
+          <button class="ui teal button">Choose Plan</button>
         </div>
         {/* End of Standard Package */}
 
-        <div className="landing_package3">
+        <div className={styles.pricing_package3}>
           <h4>Enterprise</h4>
-          <p className="landing_price2">
+          <p className={styles.price2}>
             <span>$12</span>
             <br />
             (One-time payment)
           </p>
 
-          <div className="landing_pricing_content3">
-            <div className="landing_pricing_description2 row">
+          <div className={styles.pricing_content3}>
+            <div className={styles.pricing_description2}>
               <img src="../../assets/img/check.svg" alt="Check Mark" />
-              <p className="landing_pricing_point1">Create 1 Organization</p>
+              <p className={styles.pricing_point1}>Create 1 Organization</p>
             </div>
-            <div className="landing_pricing_description3 row">
+            <div className={styles.pricing_description3}>
               <img src="../../assets/img/check.svg" alt="Check Mark" />
-              <p className="landing_pricing_point1">
+              <p className={styles.pricing_point1}>
                 Get unlimited member access for the organization
               </p>
             </div>
-            <div className="landing_pricing_description4 row">
+            <div className={styles.pricing_description4}>
               <img src="../../assets/img/check.svg" alt="Check Mark" />
-              <p className="landing_pricing_point1">
+              <p className={styles.pricing_point1}>
                 Create 100 groups under the organization
               </p>
             </div>
-            <div className="landing_pricing_description5 row">
+            <div className={styles.pricing_description5}>
               <img src="../../assets/img/check.svg" alt="Check Mark" />
-              <p className="landing_pricing_point1">
+              <p className={styles.pricing_point1}>
                 Add maximum 20 members per group
               </p>
             </div>
           </div>
 
-          <button type="button" className="btn-primary">Choose Plan</button>
+          <button class="ui white button">Choose Plan</button>
         </div>
       </div>
       {/* End of Pricing Plan Packages */}
 
-      <div className="landing_package4_bg">
-        <div className="landing_package4">
+      <div className={styles.pricing_package4_bg}>
+        <div className={styles.pricing_package4}>
           <h3>Partnership</h3>
-          <p className="landing_package4_description">
+          <p className={styles.pricing_package4_description}>
             Request a custom Organization plan and pay as you go
           </p>
-          <button type="button" className="secondary_btn">Start a plan</button>
+          {/* <button type="button" className="secondary_btn">
+            Start a plan
+          </button> */}
+          <button class="ui teal button">Start a plan</button>
         </div>
       </div>
     </div>
